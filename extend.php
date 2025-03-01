c021fec13601c63174fc0e6d903c4479599b9960
小小白哦 <107790186+odoorbell@users.noreply.github.com>
2025-03-01 19:20:23 +0800

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace odoorbell\lxgw-wenkai-font;
+namespace odoorbell\lxgwwenkaifont;
 
 use Flarum\Extend;
 use Flarum\Frontend\Document;
@@ -9,5 +9,8 @@ return [
     (new Extend\Frontend('forum'))
         ->css(__DIR__.'/less/forum.less'),  
     (new Extend\Frontend('admin'))
-        ->css(__DIR__.'/less/admin.less'),  
+        ->css(__DIR__.'/less/admin.less'), 
+    ->content(function (Document $document) {
+            $document->head[] = '<link href="https://cdn.staticfile.org/lxgw-wenkai-screen-webfont/1.7.0/lxgwwenkaiscreen.css" rel="stylesheet">';
+        })
 ];
